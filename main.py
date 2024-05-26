29b422df748bfff5fee41213348ba8b06a4bc8ca
qdilmac <shadeofficial7@outlook.com>
2024-05-26 22:21:10 +0300

    made changes for resizing the photos to 640x640

--- main.py
+++ main.py
@@ -12,46 +12,34 @@ def rotate_image(image, angle):
     result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
     return result
 
+# -> Fotoğrafları 640x640 boyutuna getirmek için kullanılan fonksiyon
+def resize_image(image):
+    image = cv2.resize(image, (640, 640))
+    return image
+
 # -> Veri setinin bulunduğu klasör yolu
 path = "dataset"
 
 # -> Klasördeki sınıfların listesi -> alt klasörler
 classes = os.listdir(path)
 
-# -> Alt klasörlerdeki resimlerin döndürülmesi
+# -> Alt klasörlerdeki resimlerin yeniden boyutlandırılması, dikeyde ve yatayda döndürülmesi
 
-for class_name in classes:
-    # -> Sınıfın klasör yolu
-    class_path = os.path.join(path, class_name)
-    # -> Klasördeki resimlerin listesi
-    images = os.listdir(class_path)
-    # -> Her bir resmi döndür
-    for image_name in images:
-        # -> Resmin yolu
-        image_path = os.path.join(class_path, image_name)
-        # -> Resmi oku
-        image = cv2.imread(image_path)
-        # -> Resmi 90 derece döndür
-        rotated_image = rotate_image(image, 90)
-        # -> Döndürülmüş resmin yolu
-        rotated_image_path = os.path.join(class_path, image_name
-                                             .replace(".png", "_rotated_90.png")) # UZANTI ÖNEMLİ!!
+for i in classes:
+    print(f"{i} sınıfı için işlem yapılıyor.")
+    images = os.listdir(os.path.join(path, i))
+    for j in images:
+        image = cv2.imread(os.path.join(path, i, j))
+        image = resize_image(image)
+        cv2.imwrite(os.path.join(path, i, "resized_" + j), image)
+        for angle in [90, 180, 270]:
+            rotated_image = rotate_image(image, angle)
+            cv2.imwrite(os.path.join(path, i, "rotated_" + str(angle) + "_" + j), rotated_image)
+    print(f"{i} sınıfı için işlem tamamlandı.")
 
-        cv2.imwrite(rotated_image_path, rotated_image)
 
-        rotated_image = rotate_image(image, 180)
 
-        rotated_image_path = os.path.join(class_path, image_name
-                                             .replace(".png", "_rotated_180.png"))
 
-        cv2.imwrite(rotated_image_path, rotated_image)
-
-        rotated_image = rotate_image(image, 270)
-
-        rotated_image_path = os.path.join(class_path, image_name
-                                                .replace(".png", "_rotated_270.png"))
-
-        cv2.imwrite(rotated_image_path, rotated_image)
 
         
 print("Resimler üzerinde döndürme işlemi tamamlandı.")
